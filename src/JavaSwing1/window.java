9c7dca384c331e2b2e13d498aa74a1c7763848a6
yaniragl <yanira_galeas@hotmail.com>
2017-11-08 21:10:15 -0300

    Declarar elementos a usar

---
+++ src/JavaSwing1/window.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaSwing1;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author ceduc
+ */
+public class window extends JFrame
+{
+   JPanel panel;
+   JLabel label;
+   JTextField text;
+   JButton boton; 
+   JCheckBox verificacion;
+   JRadioButton presionar;
+
+        
+
+{
+    
+}
