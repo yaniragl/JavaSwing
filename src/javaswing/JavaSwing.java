ad10d5f8f5f0d4014e246afc8e8421c6736fa527
yaniragl <yanira_galeas@hotmail.com>
2017-11-08 21:58:43 -0300

    Crear titulo al proyecto

--- src/javaswing/JavaSwing.java
+++ src/javaswing/JavaSwing.java
@@ -3,19 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaswing;
-
+ package javaswing;
+  import JavaSwing1.window;
+  import javax.swing.JFrame;
 /**
  *
  * @author ceduc
  */
 public class JavaSwing {
+    
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {    
+    
+         window ventana = new window();
+        ventana.setVisible(true);
+        ventana.setBounds(0, 0, 600,800);
+       
+        ventana.setTitle("Mi primer Interfaz Grafica");
+        
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
     
 }
